16891f37096c066f4d71c5bd935114bd254e237e
vichargrave <vichargrave@gmail.com>
2013-11-02 15:50:33 -0700

    Added code to build ab XML document.

--- src/com/vichargrave/ParseTest.java
+++ src/com/vichargrave/ParseTest.java
@@ -5,7 +5,7 @@
 
    ------------------------------------------
 
-   Copyright © 2013 [Vic Hargrave - http://vichargrave.com]
+   Copyright ï¿½ 2013 [Vic Hargrave - http://vichargrave.com]
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ public class ParseTest {
     public static void main(String[] args) {
         ParseTest test = new ParseTest();
         try {
+            // Parse the given document
             XmlDomDocument doc = new XmlDomDocument("./bookstore.xml");
             int count = doc.getChildCount("bookstore", 0, "book");
             for (int i = 0; i < count; i++) {
@@ -35,6 +36,25 @@ public class ParseTest {
                 System.out.println("book year     - "+doc.getChildValue("book", i, "year", 0));
                 System.out.println("book price    - "+doc.getChildValue("book", i, "price", 0));
             }
+            
+            // Build the same document programmatically
+            XmlDomDocument newdoc = new XmlDomDocument();
+            newdoc.addChildElement(null, 0, "bookstore", null);
+            newdoc.addChildElement("bookstore", 0, "book", null);
+            newdoc.setAttributeValue("book", 0, "category", "cooking");
+            newdoc.addChildElement("book", 0, "title", "Everyday Italian");
+            newdoc.setAttributeValue("title", 0, "lang", "en");
+            newdoc.addChildElement("book", 0, "author", "Giada De Laurentis");
+            newdoc.addChildElement("book", 0, "year", "2005");
+            newdoc.addChildElement("book", 0, "price", "30.00");
+            newdoc.addChildElement("bookstore", 0, "book", null);
+            newdoc.setAttributeValue("book", 1, "category", "children");
+            newdoc.addChildElement("book", 1, "title", "Harry Potter and the Half-Blood Prince");
+            newdoc.setAttributeValue("title", 1, "lang", "en");
+            newdoc.addChildElement("book", 1, "author", "J. K. Rowling");
+            newdoc.addChildElement("book", 1, "year", "2005");
+            newdoc.addChildElement("book", 1, "price", "29.99");
+            System.out.println(newdoc.renderXml());
         }
         catch (Exception ex) {
             ex.printStackTrace();
