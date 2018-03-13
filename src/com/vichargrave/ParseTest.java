d01121f5407097c77a9a6515bbc7e995ae881377
Vic Hargrave <vichargrave@gmail.com>
2018-03-12 21:29:01 -0700

    Change package name from com.vichargrave to com.example

--- src/com/vichargrave/ParseTest.java
+++ src/com/vichargrave/ParseTest.java
@@ -19,7 +19,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-package com.vichargrave;
+package com.example;
 
 public class ParseTest {
     public static void main(String[] args) {
